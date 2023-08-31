fb822bb666314bf1d9518a930202a950a2ea1118
Saul Martinez <78560085+Saul-M@users.noreply.github.com>
2023-08-31 17:14:29 +0000

    commit

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 
-const description = "I'm a Junior at Fresno State Univerity hopefully compeleting my B.A. in Fall 2024."; 
+const description =
+  "I'm a Junior at Fresno State Univerity hopefully compeleting my B.A. in Fall 2024.";
 const Education = () => {
     return (
         <section className="light" id="education">
             <h2 style={{ textAlign: "center" }}>Education</h2>
             <p className="large">{description}</p>
         </section>
-    )
+    );
 }
 
 export default Education;
